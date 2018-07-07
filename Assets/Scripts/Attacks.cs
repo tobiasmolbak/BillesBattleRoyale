4af8011e77fb6bff8538178c3ba18f61584fb551
Tobias Hvass Mølbak <tobias@molbak.org>
2018-07-08 00:54:30 +0200

    Built various animations

---
+++ Assets/Scripts/Attacks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask notToHit;
+    public Rigidbody2D projectile;
+    public float projectileSpeed = 20;
+    public string shootButton = "Fire1";
+
+    private float timeToFire = 0;
+    private Transform firePoint;
+    private PlayerPlatformerController playerCtrl;
+
+	// Use this for initialization
+	void Awake () {
+        firePoint = this.gameObject.transform.Find("ShootPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown(shootButton)) {
+            Rigidbody2D bulletInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+            bulletInstance.velocity = new Vector2(projectileSpeed, 0);
+        }
+	}
+}
--- Assets/Scripts/PlayerPlatformerController.cs
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -5,34 +5,60 @@ using UnityEngine;
 public class PlayerPlatformerController : PhysicsObject {
 
     public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
+    public float jumpTakeOffSpeed = 20;
 
-    private bool facingRight = true;
+    protected bool facingRight = true;
+    private bool prevGrounded;
+    private bool doubleJumpReady = false;
     private SpriteRenderer mSpriteRenderer;
     private Animator mAnimator;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         mSpriteRenderer = GetComponent<SpriteRenderer>();
         mAnimator = GetComponent<Animator>();
-	}
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+
+        }
+    }
 
     protected override void ComputeVelocity() {
         Vector2 move = Vector2.zero;
 
         move.x = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && grounded) {
-            velocity.y = jumpTakeOffSpeed;
+        // Jump / Double jump
+        if (Input.GetButtonDown("Jump") && (grounded || doubleJumpReady)) {
+            if (grounded) {
+                doubleJumpReady = true;
+
+                velocity.y = jumpTakeOffSpeed;
+                mAnimator.SetFloat("Jump", Mathf.Abs(velocity.y));
+
+            } /*else if (doubleJumpReady) {
+                doubleJumpReady = false;
+
+                mAnimator.SetTrigger("Jump2");
+                velocity.y = velocity.y + jumpTakeOffSpeed;
+            }*/
         }
 
-        // Letting go of jump button stops the acceleration
-        /*else if (Input.GetButtonUp("Jump")) {
-            if (velocity.y > 0) {
-                velocity.y = velocity.y * .5f;
-            }
-        }*/
+        // Landing
+        if (grounded && !prevGrounded) {
+            mAnimator.SetFloat("Jump", Mathf.Abs(velocity.y));
+            doubleJumpReady = false;
+        }
 
+        // Falling
+        if (!grounded && velocity.y < 0) {
+            mAnimator.SetTrigger("Falling");
+            mAnimator.SetFloat("Jump", Mathf.Abs(velocity.y));
+        }
+
+        // Walking
         if (velocity.x > 0 && grounded) {
             mAnimator.SetFloat("Walk", Mathf.Abs(velocity.x));
         } else if (velocity.x <= 0 && grounded) {
@@ -42,6 +68,8 @@ public class PlayerPlatformerController : PhysicsObject {
         targetVelocity = move * maxSpeed;
 
         Flip(move);
+
+        prevGrounded = grounded;
     }
 
     private void Flip(Vector2 move) {
