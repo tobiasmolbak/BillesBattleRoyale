6af5a7988df8a6c11acbd178cb96d57c4d74e391
Tobias Hvass Mølbak <tobias@molbak.org>
2018-07-11 20:54:02 +0200

    Built test map and started on Multiplayer

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] playerPrefabs;
+
+    private PlayerPlatformerController[] playerFighterList;
+    private static GameController instance;
+    
+    public static GameController Instance() {
+        return instance;
+    }
+
+    public PlayerPlatformerController[] GetAllPlayers() {
+        return playerFighterList;
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+    
+    public void UpdateOpponentTanks(RTPacket _packet) {
+
+    }
+
+    public void InstantiateOpponentShells(RTPacket _packet) {
+
+    }
+
+    public void UpdateOpponentShells(RTPacket _packet) {
+
+    }
+
+    public void RegisterOpponentCollision(RTPacket _packet) {
+
+    }
+
+    public void OnOpponentDisconnected(int _peerId) {
+
+    }
+}
